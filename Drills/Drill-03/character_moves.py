7fb632273a26e788109866b61d1b9a6968887cae
JangRakChoi(laptop) <bisu235@naver.com>
2018-09-10 21:28:41 +0900

    Drill - 03 완성
    2015182040 최장락
    https://youtu.be/u6BtveOovis

--- Drills/Drill-03/character_moves.py
+++ Drills/Drill-03/character_moves.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -8,12 +9,94 @@ character = load_image('character.png')
 # fill here
 
 x = 0
+y = 90
+delta = 0
+r = 210
+num = 10
 
-while (x < 800) :
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+while (num > 0) :
+    while (x < 780) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 10
+        delay(0.01)
 
-close_canvas()
+    while ( y < 570) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y + 10
+        delay(0.01)
+
+    while ( x > 20) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x - 10
+        delay(0.01)
+
+    while (y > 90) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y - 10
+        delay(0.01)
+
+    while (x < 400) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 10
+        delay(0.01)
+    
+    while (x < 700 and y < 300) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 4
+        delta = math.sqrt((400 - x)**2 + (300 - y)**2) 
+        y = y + (delta - r)
+        if (x > 700) :
+            x = 700
+        delay(0.01)
+        
+    while (x > 400 and y < 600) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y + 4
+        delta = math.sqrt((400 - x)**2 + (300 - y)**2)
+        if ( r > delta) :
+            x = x - (r - delta)
+        else :
+            x = x - (delta - r)
+        delay(0.01)
+    
+    while (x > 0 and y > 300) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x - 4
+        delta = math.sqrt((400 - x)**2 + (300 - y)**2) 
+        if ( r > delta) :
+            y = y - (r - delta)
+        else :
+            y = y - (delta - r)
+        delay(0.01)
+        
+    while (x < 400 and y > 90) :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 4
+        delta = math.sqrt((400 - x)**2 + (300 - y)**2)
+        if ( r > delta) :
+            y = y - (r - delta)
+        else :
+            y = y - (delta - r)
+        delay(0.01)
+
+    num = num - 1
+    
+close_canvas()    
