4e8715a723d0de96cce6b84f66eabf396a5f2a21
JangRakChoi(laptop) <bisu235@naver.com>
2018-09-17 18:03:18 +0900

    Drill - 05 스텝1 y축, 정지상태 뼈대

--- Drills/Drill-05/character_runs.py
+++ Drills/Drill-05/character_runs.py
@@ -3,8 +3,35 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def MoveFirstStep() :
+def StopRightSide(x, y) :
     pass
+
+def StopLeftSide(x, y) :
+    pass
+
+def MoveFirstStep() :
+    frame, x, y = 0, 203, 535
+    while (x > 132) :
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.01)
+        get_events()
+
+    while (y > 243) :
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y -= 5
+        delay(0.01)
+        get_events()
+
+    StopRightSide(x, y)
 
 def MoveSecondStep() :
     pass
